655cacc53ae6a7236f705a56b9befb8fa9478a23
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-12 18:50:04 +0330

    feat(api): add new routs

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,15 +1,37 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get("/api/health", (c) => {
+  return c.json({ status: "ok", message: "apis" });
+});
+
+app.get("/api/transactions/:id", (c) => {
+  const id = c.req.param("id");
+  return c.json({
+    id,
+    message: "transaction found",
+  });
+});
+app.get("/api/transactions", (c) => {
+  const { type, category } = c.req.query();
+  return c.json({
+    type,
+    category,
+  });
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
