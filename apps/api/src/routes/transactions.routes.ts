b348489f1d7208072836c90227ed622ef8d243ee
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-13 17:50:28 +0330

    feat(api): add new api route for post transactions

--- apps/api/src/routes/transactions.routes.ts
+++ apps/api/src/routes/transactions.routes.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import type { Transaction } from "../types/transaction.js";
 
 const transactionsRoutes = new Hono();
 
@@ -17,4 +18,11 @@ transactionsRoutes.get(":id", (c) => {
     message: "transaction found",
   });
 });
+
+transactionsRoutes.post("/", async (c) => {
+  const body = await c.req.json<Transaction>();
+
+  return c.json(body, 201);
+});
+
 export default transactionsRoutes;
