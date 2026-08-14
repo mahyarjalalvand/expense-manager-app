d434359b26e9592745d95ae023da6d5e3c71fc19
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-14 12:24:43 +0330

    add(api): drizzle config file

---
+++ apps/api/drizzle.config.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./src/db/schema/",
+  dialect: "postgresql",
+  out: "./drizzle",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
