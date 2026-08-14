a228bcb9174d16f771afd5b83ce8b34a42bd1207
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-14 13:40:36 +0330

    add(api): query for read all transactions in /. and add: db insert for post method

--- apps/api/src/routes/transactions.routes.ts
+++ apps/api/src/routes/transactions.routes.ts
@@ -1,28 +1,37 @@
 import { Hono } from "hono";
 import type { Transaction } from "../types/transaction.js";
+import { db } from "../db/index.js";
+import { transactions } from "../db/schema/transactions.js";
+import { eq } from "drizzle-orm";
 
 const transactionsRoutes = new Hono();
 
-transactionsRoutes.get("/", (c) => {
-  const { type, category } = c.req.query();
-  return c.json({
-    type,
-    category,
-  });
+transactionsRoutes.get("/", async (c) => {
+  const result = await db.select().from(transactions);
+  return c.json(result);
 });
 
-transactionsRoutes.get(":id", (c) => {
+transactionsRoutes.get("/:id", async (c) => {
   const id = c.req.param("id");
-  return c.json({
-    id,
-    message: "transaction found",
-  });
+  const result = await db.select().from(transactions).where(eq(transactions.id, id));
+
+  return c.json(result[0]);
 });
 
 transactionsRoutes.post("/", async (c) => {
   const body = await c.req.json<Transaction>();
 
-  return c.json(body, 201);
+  const result = await db
+    .insert(transactions)
+    .values({
+      title: body.title,
+      amount: body.amount,
+      category: body.category,
+      type: body.type,
+    })
+    .returning();
+
+  return c.json(result[0], 201);
 });
 
 export default transactionsRoutes;
