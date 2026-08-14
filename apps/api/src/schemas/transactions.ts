c12f87ec3172243c06c4e33547fa7c594b925ae1
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-14 16:48:13 +0330

    feat(api): add schema for transactions

---
+++ apps/api/src/schemas/transactions.ts
@@ -0,0 +1,8 @@
+import z from "zod";
+
+export const createTransactionSchema = z.object({
+  title: z.string().min(1),
+  amount: z.number(),
+  category: z.string().min(1),
+  type: z.enum(["income", "expense"]),
+});
