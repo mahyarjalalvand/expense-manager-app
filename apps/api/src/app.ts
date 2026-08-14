6aa27f108c7581f59d713d7dcc7cf9be1f393169
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-14 19:19:30 +0330

    feat(api): handle cors

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,8 +1,11 @@
 import { Hono } from "hono";
 import healthRoutes from "./routes/health.routes.js";
 import transactionsRoutes from "./routes/transactions.routes.js";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+
+app.use("/api/*", cors({ origin: "http://localhost:5173" }));
 
 app.route("/api/health", healthRoutes);
 app.route("/api/transactions", transactionsRoutes);
