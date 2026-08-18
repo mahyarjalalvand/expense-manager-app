8d16688a0a46371e21a54650257fff7bd6244622
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-18 19:52:19 +0330

    add(web): functions for query client. add: queryclientprovider wrapper in app component

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -7,17 +7,21 @@ import Dashboard from "./pages/Dashboard";
 import Categories from "./pages/Categories";
 import Setting from "./pages/Setting";
 import Transactions from "./pages/Transactions";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
 
 function App() {
   return (
-    <AppLayout>
-      <Routes>
-        <Route path={routes.dashboard} element={<Dashboard />} />
-        <Route path={routes.categories} element={<Categories />} />
-        <Route path={routes.setting} element={<Setting />} />
-        <Route path={routes.transactions} element={<Transactions />} />
-      </Routes>
-    </AppLayout>
+    <QueryClientProvider client={queryClient}>
+      <AppLayout>
+        <Routes>
+          <Route path={routes.dashboard} element={<Dashboard />} />
+          <Route path={routes.categories} element={<Categories />} />
+          <Route path={routes.setting} element={<Setting />} />
+          <Route path={routes.transactions} element={<Transactions />} />
+        </Routes>
+      </AppLayout>
+    </QueryClientProvider>
   );
 }
 
