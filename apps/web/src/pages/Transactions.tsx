00de29abf7f5cc0c3323ad13c1f6e08d75a2a073
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-18 19:52:50 +0330

    add(web): table component and show transactions data

--- apps/web/src/pages/Transactions.tsx
+++ apps/web/src/pages/Transactions.tsx
@@ -1,9 +1,28 @@
+import { getAllTransactions } from "@/api/transactions";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { useQuery } from "@tanstack/react-query";
+
 function Transactions() {
+  const { data: transactions, isError, isLoading } = useQuery({ queryKey: ["transactions"], queryFn: getAllTransactions });
+  console.log(transactions);
   return (
-    <div>
-      <h1 className="text-2xl font-semibold">transactions</h1>
-      <p>manage youre transactions</p>
-    </div>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Date</TableHead>
+          <TableHead>Time</TableHead>
+          <TableHead>Category</TableHead>
+          <TableHead>Amount</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {transactions?.map((item) => (
+          <TableRow key={item.id}>
+            <TableCell>{item.amount}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
 }
 
