9ac6545e14cf7237a33b311b10f41d11a1e82b1d
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-18 22:14:23 +0330

    feat(web): toaster in app compo

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -9,6 +9,7 @@ import Setting from "./pages/Setting";
 import Transactions from "./pages/Transactions";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
+import { Toaster } from "sonner";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path={routes.setting} element={<Setting />} />
           <Route path={routes.transactions} element={<Transactions />} />
         </Routes>
+        <Toaster />
       </AppLayout>
     </QueryClientProvider>
   );
