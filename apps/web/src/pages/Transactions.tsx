7b4363cfeb77009a74dc3137da503ddd6f0af658
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-25 22:23:51 +0330

    add(web): transactions filter component in transactions page

--- apps/web/src/pages/Transactions.tsx
+++ apps/web/src/pages/Transactions.tsx
@@ -1,15 +1,20 @@
 import CreateTransactionDialog from "@/components/CreateTransactionDialog";
 import { Button } from "@/components/ui/button";
+import TransactionsFilter from "@/components/ui/TransactionsFilter";
 import TransactionsTable from "@/components/ui/TransactionsTable";
 
 import { useTransactions } from "@/hooks/useTransactions";
+import type { TransactionsFilterState } from "@/types/transactions.type";
 import { Loader2, PlusCircleIcon } from "lucide-react";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
 function Transactions() {
   const [open, setOpen] = useState(false);
+  const [transactionFilter, setTransactionFilter] = useState<TransactionsFilterState>("all");
+
   const { data: transactions, isError, isLoading, error } = useTransactions();
+
   useEffect(() => {
     if (isError) {
       toast.error(error.message);
@@ -18,10 +23,13 @@ function Transactions() {
 
   return (
     <section>
-      <Button variant={"outline"} onClick={() => setOpen(true)} className="mb-4 flex items-center gap-2">
-        Add Transaction
-        <PlusCircleIcon />
-      </Button>
+      <div className="flex w-full items-center justify-between gap-3">
+        <Button variant={"outline"} onClick={() => setOpen(true)} className="mb-4 flex items-center gap-2">
+          Add Transaction
+          <PlusCircleIcon />
+        </Button>
+        <TransactionsFilter filter={transactionFilter} setFilter={setTransactionFilter} />
+      </div>
       <div className="bg-background overflow-hidden rounded-xl border">
         {isLoading ? (
           <div className="center h-60">
