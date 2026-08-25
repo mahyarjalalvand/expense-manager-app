e8d5dcc308360ee72a6b1bfff0ccf659f5b190ba
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-25 22:21:01 +0330

    feat(web): filter button ui in transactions page

---
+++ apps/web/src/components/ui/TransactionsFilter.tsx
@@ -0,0 +1,33 @@
+import type { Dispatch, SetStateAction } from "react";
+import { ToggleGroup, ToggleGroupItem } from "./toggle-group";
+import type { TransactionsFilterState } from "@/types/transactions.type";
+type TransactionsFilterProps = {
+  filter: TransactionsFilterState;
+  setFilter: Dispatch<SetStateAction<TransactionsFilterState>>;
+};
+function TransactionsFilter({ filter, setFilter }: TransactionsFilterProps) {
+  return (
+    <ToggleGroup
+      variant="outline"
+      value={[filter]}
+      onValueChange={(value) => {
+        const selectedFilter = value[0];
+
+        if (selectedFilter) {
+          setFilter(selectedFilter as TransactionsFilterState);
+        }
+      }}>
+      <ToggleGroupItem value="all" aria-label="Toggle all">
+        All
+      </ToggleGroupItem>
+      <ToggleGroupItem value="expense" aria-label="Toggle expense">
+        Expense
+      </ToggleGroupItem>
+      <ToggleGroupItem value="income" aria-label="Toggle income">
+        Income
+      </ToggleGroupItem>
+    </ToggleGroup>
+  );
+}
+
+export default TransactionsFilter;
