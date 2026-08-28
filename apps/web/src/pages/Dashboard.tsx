b2f13498a59c44b4147d4a3bf29db2f58dc42178
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-28 22:31:35 +0330

    add(web): useDashboard hook and add summary card

--- apps/web/src/pages/Dashboard.tsx
+++ apps/web/src/pages/Dashboard.tsx
@@ -1,8 +1,23 @@
+import SummaryCard from "@/components/ui/SummaryCard";
+import { useDashboard } from "@/hooks/useDashboard";
+
 function Dashboard() {
+  const { data, isPending, isError } = useDashboard();
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Failed to load dashboard</div>;
+  }
   return (
     <div>
-      <h1 className="text-2xl font-bold">Dashboard</h1>
       <p className="text-muted-foreground">overview of your financial activity</p>
+      <div className="w-full grid gap-4 md:grid-cols-3 mt-5">
+        <SummaryCard title="Income" value={data?.summary.income} />
+        <SummaryCard title="Expense" value={data?.summary.expenses} />
+        <SummaryCard title="Balance" value={data?.summary.balance} />
+      </div>
     </div>
   );
 }
