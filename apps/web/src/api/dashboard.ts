111bb246cd6de0f152dce1ac34cd5ed7e76766fa
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-28 20:08:10 +0330

    complete(web): getDashboard func

--- apps/web/src/api/dashboard.ts
+++ apps/web/src/api/dashboard.ts
@@ -1,9 +1,13 @@
+import type { DashboardData } from "@/types/dashboard";
+
 const baseUrl = import.meta.env.VITE_BASE_URL;
 
-export const getDashboard = async () => {
+export const getDashboard = async (): Promise<DashboardData> => {
   const res = await fetch(`${baseUrl}dashboard`);
   if (!res.ok) {
     throw new Error("Failed to fetch dashboard data");
   }
-  return res.json();
+  const data = await res.json();
+  console.log(data);
+  return data;
 };
