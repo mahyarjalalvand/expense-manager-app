1ca0025361e230f17884d6742de89e1f5cb0e649
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-28 19:28:31 +0330

    add(api): returned dashboard data in json

--- apps/api/src/routes/dashboard.routes.ts
+++ apps/api/src/routes/dashboard.routes.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono";
+import { getDashboard } from "../services/dashboard.service.js";
 
 const dashboardRoutes = new Hono();
 
-dashboardRoutes.get("/", async () => {});
-
+dashboardRoutes.get("/", async (c) => {
+  const result = await getDashboard();
+  return c.json(result);
+});
 export default dashboardRoutes;
