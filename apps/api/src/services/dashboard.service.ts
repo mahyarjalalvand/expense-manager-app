872cfa9dcac67223591e44b2e2fffb7a0d0325c1
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-28 19:26:31 +0330

    add(api): query for incomeEpenseBy Day

--- apps/api/src/services/dashboard.service.ts
+++ apps/api/src/services/dashboard.service.ts
@@ -1,8 +1,59 @@
-import { desc } from "drizzle-orm";
+import { and, desc, gte, lt, sql } from "drizzle-orm";
 import { db } from "../db/index.js";
 import { transactions } from "../db/schema/transactions.js";
+import { formatDateKey } from "../utils/formatDate.js";
 
-export const getRecentTransactions = async () => {
-  const result = db.select().from(transactions).orderBy(desc(transactions.createdAt)).limit(5);
-  return result;
+export const getDashboard = async () => {
+  const now = new Date();
+
+  const startDate = new Date(now.getFullYear(), now.getMonth(), 1);
+  const endDate = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+
+  const result = await db
+    .select({
+      income: sql<number>`COALESCE(SUM(CASE WHEN ${transactions.type} = 'income' THEN ${transactions.amount} ELSE 0 END),0)`,
+      expenses: sql<number>`COALESCE(SUM(CASE WHEN ${transactions.type} = 'expense' THEN ${transactions.amount} ELSE 0 END),0)`,
+    })
+    .from(transactions)
+    .where(and(gte(transactions.createdAt, startDate), lt(transactions.createdAt, endDate)));
+
+  const incomeExpenseByDay = await db
+    .select({
+      date: sql<string>`DATE_TRUNC('day' , ${transactions.createdAt})`,
+      income: sql<number>`COALESCE(SUM(CASE WHEN ${transactions.type} = 'income' THEN ${transactions.amount} ELSE 0 END), 0)`,
+      expense: sql<number>`COALESCE(SUM(CASE WHEN ${transactions.type} = 'expense' THEN ${transactions.amount} ELSE 0 END), 0)`,
+    })
+    .from(transactions)
+    .where(and(gte(transactions.createdAt, startDate), lt(transactions.createdAt, endDate)))
+    .groupBy(sql`DATE_TRUNC('day', ${transactions.createdAt})`)
+    .orderBy(sql`DATE_TRUNC('day' , ${transactions.createdAt})`);
+
+  const { expenses, income } = result[0];
+
+  const incomeNumber = Number(income);
+  const expenseNumber = Number(expenses);
+
+  const dailyDate = incomeExpenseByDay.map((item) => ({
+    date: item.date.slice(0, 10),
+    income: Number(item.income),
+    expense: Number(item.expense),
+  }));
+
+  const dailyDateMap = new Map(dailyDate.map((item) => [item.date, item]));
+
+  const completeDailyDate = [];
+  for (let date = new Date(startDate); date < endDate; date.setDate(date.getDate() + 1)) {
+    const dateKey = formatDateKey(date);
+    const data = dailyDateMap.get(dateKey);
+
+    completeDailyDate.push({
+      date: dateKey,
+      income: data?.income ?? 0,
+      expense: data?.expense ?? 0,
+    });
+  }
+
+  const recentTransactions = await db.select().from(transactions).orderBy(desc(transactions.createdAt)).limit(5);
+
+  return { summary: { expenses: expenseNumber, income: incomeNumber, balance: incomeNumber - expenseNumber }, dailyData: completeDailyDate, recentTransactions };
 };
