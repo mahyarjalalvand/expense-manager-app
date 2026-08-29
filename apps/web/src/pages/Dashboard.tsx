52a82c28c92bc0c83d2fb76b3ac0fba6d74e8887
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-29 19:51:37 +0330

    add(web): variant and icon for summary card

--- apps/web/src/pages/Dashboard.tsx
+++ apps/web/src/pages/Dashboard.tsx
@@ -2,6 +2,7 @@ import IncomeExpenseChart from "@/components/ui/IncomeExpenseChart";
 import RecentTransactions from "@/components/ui/RecentTransactions";
 import SummaryCard from "@/components/ui/SummaryCard";
 import { useDashboard } from "@/hooks/useDashboard";
+import { ArrowDownLeft, ArrowUpRight, Wallet } from "lucide-react";
 
 function Dashboard() {
   const { data, isPending, isError } = useDashboard();
@@ -16,9 +17,9 @@ function Dashboard() {
     <div>
       <p className="text-muted-foreground">overview of your financial activity</p>
       <div className="w-full grid gap-4 md:grid-cols-3 mt-5">
-        <SummaryCard title="Income" value={data?.summary.income} description="Total income this month" />
-        <SummaryCard title="Expense" value={data?.summary.expenses} description="Total expenses this month" />
-        <SummaryCard title="Balance" value={data?.summary.balance} description="Remaining balance this month" />
+        <SummaryCard title="Income" variant="income" icon={ArrowDownLeft} value={data?.summary.income} description="Total income this month" />
+        <SummaryCard title="Expense" variant="expense" icon={ArrowUpRight} value={data?.summary.expenses} description="Total expenses this month" />
+        <SummaryCard title="Balance" variant="balance" icon={Wallet} value={data?.summary.balance} description="Remaining balance this month" />
       </div>
       <div className="mt-6 w-full min-w-0 overflow-hidden">
         <IncomeExpenseChart data={data.dailyData} />
