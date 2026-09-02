6d54fbc56c54baa2dd91e207308efaec506868cf
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-09-02 10:45:55 +0330

    add(web): card component as a wrapper

--- apps/web/src/components/ui/IncomeExpenseChart.tsx
+++ apps/web/src/components/ui/IncomeExpenseChart.tsx
@@ -1,4 +1,5 @@
 import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import { Card, CardHeader, CardTitle } from "./card";
 
 type InconeExpenseChartProps = {
   data: {
@@ -14,31 +15,37 @@ const formatAmount = (value: number) => {
 
 function IncomeExpenseChart({ data }: InconeExpenseChartProps) {
   return (
-    <ResponsiveContainer width="100%" height={350} minWidth={0}>
-      <LineChart data={data}>
-        <CartesianGrid />
-        <XAxis
-          dataKey="date"
-          tickFormatter={(date) => {
-            return new Date(date).toLocaleDateString("en-US", {
-              month: "short",
-              day: "numeric",
-            });
-          }}
-        />
-        <YAxis />
-        <Tooltip
-          formatter={(value, name) => [formatAmount(Number(value)), name === "income" ? "Income" : "Expense"]}
-          allowEscapeViewBox={{
-            x: false,
-            y: false,
-          }}
-        />
-        <Legend />
-        <Line type="monotone" dataKey="income" />
-        <Line type="monotone" dataKey="expense" />
-      </LineChart>
-    </ResponsiveContainer>
+    <Card>
+      <CardHeader>
+        <CardTitle>Income & Expenses</CardTitle>
+        <p></p>
+      </CardHeader>
+      <ResponsiveContainer width="100%" height={350} minWidth={0}>
+        <LineChart data={data}>
+          <CartesianGrid />
+          <XAxis
+            dataKey="date"
+            tickFormatter={(date) => {
+              return new Date(date).toLocaleDateString("en-US", {
+                month: "short",
+                day: "numeric",
+              });
+            }}
+          />
+          <YAxis />
+          <Tooltip
+            formatter={(value, name) => [formatAmount(Number(value)), name === "income" ? "Income" : "Expense"]}
+            allowEscapeViewBox={{
+              x: false,
+              y: false,
+            }}
+          />
+          <Legend />
+          <Line type="monotone" dataKey="income" />
+          <Line type="monotone" dataKey="expense" />
+        </LineChart>
+      </ResponsiveContainer>
+    </Card>
   );
 }
 
