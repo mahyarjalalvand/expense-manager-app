d3bef7b38040422be113b5f847b8971d7a239591
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-09-03 19:04:45 +0330

    add(api): utility function for calc date range

---
+++ apps/api/src/utils/getDateRange.ts
@@ -0,0 +1,39 @@
+import type { DateRange } from "../schemas/dateRange.js";
+
+export const getDateRange = (range: DateRange) => {
+  const now = new Date();
+  switch (range) {
+    case "7d": {
+      const startDate = new Date(now);
+      startDate.setDate(now.getDate() - 7);
+      return {
+        startDate,
+        endDate: now,
+      };
+    }
+    case "30d": {
+      const startDate = new Date(now);
+      startDate.setDate(now.getMonth() - 30);
+      return {
+        startDate,
+        endDate: now,
+      };
+    }
+    case "month": {
+      const startDate = new Date(now.getFullYear(), now.getMonth(), 1);
+      const endDate = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+      return {
+        startDate,
+        endDate,
+      };
+    }
+    case "year": {
+      const startDate = new Date(now.getFullYear(), 0, 1);
+      const endDate = new Date(now.getFullYear() + 1, 0, 1);
+      return {
+        startDate,
+        endDate,
+      };
+    }
+  }
+};
