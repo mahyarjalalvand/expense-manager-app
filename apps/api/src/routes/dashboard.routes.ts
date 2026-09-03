a7a273d1114a1db5944b24dd3f9f9b0d65a4fd67
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-09-03 19:05:44 +0330

    feat(apa): get range query and hanle error

--- apps/api/src/routes/dashboard.routes.ts
+++ apps/api/src/routes/dashboard.routes.ts
@@ -1,10 +1,21 @@
 import { Hono } from "hono";
 import { getDashboard } from "../services/dashboard.service.js";
+import { dateRangeSchema } from "../schemas/dateRange.js";
 
 const dashboardRoutes = new Hono();
 
 dashboardRoutes.get("/", async (c) => {
-  const result = await getDashboard();
+  const range = c.req.query("range");
+  const parsedRange = dateRangeSchema.safeParse(range);
+  if (!parsedRange.success) {
+    return c.json(
+      {
+        message: "invalid date range",
+      },
+      400,
+    );
+  }
+  const result = await getDashboard(parsedRange.data);
   return c.json(result);
 });
 export default dashboardRoutes;
