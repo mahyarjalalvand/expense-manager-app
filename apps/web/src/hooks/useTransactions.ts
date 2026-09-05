0f98d0bf9efc03a44db1bc271800729a3c0790ad
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-09-05 18:30:38 +0330

    add(web): spread operator for query key in trasnactions

--- apps/web/src/hooks/useTransactions.ts
+++ apps/web/src/hooks/useTransactions.ts
@@ -5,7 +5,7 @@ export const transactionsQueryKey = ["transactions"];
 
 export const useTransactions = (page: number, limit: number) => {
   return useQuery({
-    queryKey: [transactionsQueryKey, page, limit],
+    queryKey: [...transactionsQueryKey, page, limit],
     queryFn: () => getAllTransactions(page, limit),
   });
 };
