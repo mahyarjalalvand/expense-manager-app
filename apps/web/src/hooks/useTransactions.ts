469c8ab08706ca422503a395446fef3d53010dcd
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-09-08 18:23:47 +0330

    feat(web): add keepPreviusData for improve transactions pagination ux

--- apps/web/src/hooks/useTransactions.ts
+++ apps/web/src/hooks/useTransactions.ts
@@ -1,5 +1,5 @@
 import { getAllTransactions } from "@/api/transactions";
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
 export const transactionsQueryKey = ["transactions"];
 
@@ -7,5 +7,6 @@ export const useTransactions = (page: number, limit: number) => {
   return useQuery({
     queryKey: [...transactionsQueryKey, page, limit],
     queryFn: () => getAllTransactions(page, limit),
+    placeholderData: keepPreviousData,
   });
 };
