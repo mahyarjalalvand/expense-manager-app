6b64151a374d83ded406ed583baae3ec1a5878df
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-09-09 16:19:23 +0330

    add(api): schema for transactions query

--- apps/api/src/schemas/transactions.ts
+++ apps/api/src/schemas/transactions.ts
@@ -9,3 +9,9 @@ export const createTransactionSchema = z.object({
 export const transactionIdSchema = z.uuid();
 
 export const updateTransactionSchema = createTransactionSchema.partial();
+
+export const transactionsQuerySchema = z.object({
+  page: z.coerce.number().int().min(1).default(1),
+  limit: z.coerce.number().int().min(1).default(10),
+  type: z.enum(["all", "expense", "income"]).default("all"),
+});
