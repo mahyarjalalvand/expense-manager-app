0ce3747453bc95e730b674457edab18808f6400a
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-09-12 18:53:37 +0330

    add(api): auth handler for auth routes

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,10 +1,15 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+
+import { auth } from "./auth.js";
+
 import healthRoutes from "./routes/health.routes.js";
 import transactionsRoutes from "./routes/transactions.routes.js";
-import { cors } from "hono/cors";
 import dashboardRoutes from "./routes/dashboard.routes.js";
 
 const app = new Hono();
+
+app.all("/api/auth/*", (c) => auth.handler(c.req.raw));
 
 app.use("/api/*", cors({ origin: "http://localhost:5173" }));
 
