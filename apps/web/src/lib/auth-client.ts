17cd5ea896fdffb32a1f6cfd48f2492896a98a32
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-09-13 18:08:34 +0330

    fix(web): base url in auth client

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,4 +1,4 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: import.meta.env.BASE_URL,
+  baseURL: import.meta.env.VITE_BASE_URL,
 });
