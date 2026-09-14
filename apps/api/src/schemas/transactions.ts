8a3d43bd8ee08b25437ce25079be89648bd737fd
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-09-14 17:39:08 +0330

    fix(api): categoryId name

--- apps/api/src/schemas/transactions.ts
+++ apps/api/src/schemas/transactions.ts
@@ -3,7 +3,7 @@ import z from "zod";
 export const createTransactionSchema = z.object({
   title: z.string().min(1),
   amount: z.number(),
-  category: z.string().min(1),
+  categoryId: z.string().min(1),
   type: z.enum(["income", "expense"]),
 });
 export const transactionIdSchema = z.uuid();
