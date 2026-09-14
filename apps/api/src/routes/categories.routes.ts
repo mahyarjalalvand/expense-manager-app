4182cfa43bd5d7d23d84c201fa461dc6816e4d62
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-09-14 19:01:38 +0330

    feat(api): handle get and post req for categories

--- apps/api/src/routes/categories.routes.ts
+++ apps/api/src/routes/categories.routes.ts
@@ -1,3 +1,23 @@
 import { Hono } from "hono";
+import { authMiddleware } from "../middlewares/auth.js";
+import { createCategory, getCategories } from "../services/categories.service.js";
+import { createCategorySchema } from "../schemas/categories.js";
 
 export const categoriesRoutes = new Hono();
+
+categoriesRoutes.get("/", authMiddleware, async (c) => {
+  const user = c.get("user");
+  const result = await getCategories(user.id);
+  return c.json(result);
+});
+
+categoriesRoutes.post("/", authMiddleware, async (c) => {
+  const user = c.get("user");
+  const body = c.req.json();
+  const parsed = createCategorySchema.safeParse(body);
+  if (!parsed.success) {
+    return c.json({ message: "Invalid category data", errors: parsed.error.message }, 400);
+  }
+  const result = await createCategory(user.id, parsed.data);
+  return c.json(result, 201);
+});
