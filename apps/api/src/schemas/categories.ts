842698e03e95bd27ddf0febde0acf6444a96977b
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-09-14 19:00:41 +0330

    add(api): schema for validate data for create category

---
+++ apps/api/src/schemas/categories.ts
@@ -0,0 +1,8 @@
+import z from "zod";
+
+export const createCategorySchema = z.object({
+  name: z.string().min(1, "Category name is required"),
+  icon: z.string().optional(),
+  color: z.string().optional(),
+});
+export type CreateCategory = z.infer<typeof createCategorySchema>;
