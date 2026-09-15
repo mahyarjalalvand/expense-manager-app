d081d63475e71dbadc0a3038da1f4ae670c9e59b
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-09-15 16:12:03 +0330

    add(api): update category function for update query

--- apps/api/src/services/categories.service.ts
+++ apps/api/src/services/categories.service.ts
@@ -1,7 +1,7 @@
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { db } from "../db/index.js";
 import { categories } from "../db/schema/categories.js";
-import type { CreateCategory } from "../schemas/categories.js";
+import type { CreateCategory, UpdateCategory } from "../schemas/categories.js";
 
 export const getCategories = async (userId: string) => {
   return db.select().from(categories).where(eq(categories.userId, userId));
@@ -9,5 +9,18 @@ export const getCategories = async (userId: string) => {
 
 export const createCategory = async (userId: string, data: CreateCategory) => {
   const result = await db.insert(categories).values({ userId, name: data.name, icon: data.icon, color: data.color }).returning();
+  return result[0];
+};
+
+export const updateCategory = async (userId: string, categoryId: string, data: UpdateCategory) => {
+  const result = await db
+    .update(categories)
+    .set({
+      ...data,
+      updatedAt: new Date(),
+    })
+    .where(and(eq(categories.id, categoryId), eq(categories.userId, userId)))
+    .returning();
+
   return result[0];
 };
