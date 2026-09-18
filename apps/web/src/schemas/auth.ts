5888654e9979693938cc89728c7ea7d54c544104
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-09-18 16:56:57 +0330

    add(web): schema for login

--- apps/web/src/schemas/auth.ts
+++ apps/web/src/schemas/auth.ts
@@ -6,3 +6,10 @@ export const registerSchema = z.object({
   password: z.string().min(8, "Password must be at least 8 characters"),
 });
 export type RegisterFormValues = z.infer<typeof registerSchema>;
+
+export const LoginSchema = z.object({
+  email: z.email("Invalid email address "),
+  password: z.string().min(1, "Password is required"),
+});
+
+export type LoginFormValues = z.infer<typeof LoginSchema>;
