5280e0ef95d41a67dbb4c89ec654e01beb79ef26
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-09-18 18:08:32 +0330

    feat(web): add logout button

--- apps/web/src/components/layout/Header.tsx
+++ apps/web/src/components/layout/Header.tsx
@@ -1,13 +1,26 @@
 import { navigation } from "@/routes/routes";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Button } from "../ui/button";
+import { LogOut } from "lucide-react";
+import { authClient } from "@/lib/auth-client";
 
 function Header() {
   const { pathname } = useLocation();
   const currentRouteTitle = navigation.find((item) => item.href === pathname)?.title;
 
+  const navigate = useNavigate();
+
+  const logoutHandler = async () => {
+    await authClient.signOut();
+    navigate("/");
+  };
+
   return (
-    <header className="flex h-16 items-center border-b bg-background px-6">
+    <header className="flex h-16 items-center justify-between border-b bg-background px-6">
       <h2 className="text-lg font-semibold">{currentRouteTitle}</h2>
+      <Button onClick={logoutHandler} className="bg-red-500 text-white">
+        <LogOut />
+      </Button>
     </header>
   );
 }
