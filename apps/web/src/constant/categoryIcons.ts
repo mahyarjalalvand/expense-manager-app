5273a05fd5d9fbb3f73e983ff5ee1ed9e1d552c4
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-09-20 11:51:45 +0330

    add(web): icons object for categories

---
+++ apps/web/src/constant/categoryIcons.ts
@@ -0,0 +1,12 @@
+import { Car, CircleDollarSign, Clapperboard, Laptop, Receipt, ShoppingBag, Utensils } from "lucide-react";
+
+export const categoryIcons = {
+  utensils: Utensils,
+  car: Car,
+  "shopping-bag": ShoppingBag,
+  receipt: Receipt,
+  clapperboard: Clapperboard,
+  laptop: Laptop,
+  briefcase: Laptop,
+  "circle-dollar-sign": CircleDollarSign,
+};
