630f35899458529a1e8e3ba10709102770d10484
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-09-20 11:52:26 +0330

    add(web): category hook for get all categories and add icons

--- apps/web/src/pages/Categories.tsx
+++ apps/web/src/pages/Categories.tsx
@@ -1,46 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { categoryIcons } from "@/constant/categoryIcons";
+import { useCategories } from "@/hooks/useCategories";
 import { MoreHorizontal, Plus } from "lucide-react";
 
-const categories = [
-  {
-    id: "1",
-    name: "Food",
-    icon: "🍔",
-    color: "#22c55e",
-  },
-  {
-    id: "2",
-    name: "Transport",
-    icon: "🚗",
-    color: "#3b82f6",
-  },
-  {
-    id: "3",
-    name: "Shopping",
-    icon: "🛍️",
-    color: "#a855f7",
-  },
-  {
-    id: "4",
-    name: "Bills",
-    icon: "🧾",
-    color: "#f97316",
-  },
-  {
-    id: "5",
-    name: "Entertainment",
-    icon: "🎬",
-    color: "#ec4899",
-  },
-];
-
 function Categories() {
+  const { data: categories, isPending } = useCategories();
+
   return (
     <div className="space-y-6">
       {/* header */}
-      <div className="flex item-center justify-between gap-4">
+      <div className="flex items-center justify-between gap-4">
         <div>
           <h1 className="text-2xl font-semibold tracking-tight">categories</h1>
           <p className="text-sm text-muted-foreground"> Manage your income and expense categories</p>
@@ -53,37 +24,40 @@ function Categories() {
       <Card>
         <CardHeader className="text-base">Your categories</CardHeader>
         <CardContent className="space-y-2">
-          {categories.map((category) => (
-            <div key={category.id} className="flex items-center justify-between rounded-lg border p-3">
-              <div className="flex items-center gap-3">
-                <div
-                  className="flex size-10 items-center justify-center rounded-lg text-lg"
-                  style={{
-                    backgroundColor: `${category.color}20`,
-                  }}>
-                  {category.icon}
-                </div>
+          {categories?.map((category) => {
+            const Icon = categoryIcons[category.icon as keyof typeof categoryIcons];
+            return (
+              <div key={category.id} className="flex items-center justify-between rounded-lg border p-3">
+                <div className="flex items-center gap-3">
+                  <div
+                    className="flex size-10 items-center justify-center rounded-lg text-lg"
+                    style={{
+                      backgroundColor: `${category.color}20`,
+                    }}>
+                    {Icon && <Icon className="size-5" />}
+                  </div>
 
-                <div>
-                  <p className="text-sm font-medium">{category.name}</p>
+                  <div>
+                    <p className="text-sm font-medium">{category.name}</p>
 
-                  <p className="text-xs text-muted-foreground">Default category</p>
+                    <p className="text-xs text-muted-foreground">Default category</p>
+                  </div>
                 </div>
+                <DropdownMenu>
+                  <DropdownMenuTrigger>
+                    <Button variant="ghost" size="icon">
+                      <MoreHorizontal />
+                    </Button>
+                  </DropdownMenuTrigger>
+
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuItem>Edit</DropdownMenuItem>
+                    <DropdownMenuItem className="text-destructive">Delete</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
               </div>
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <Button variant="ghost" size="icon">
-                    <MoreHorizontal />
-                  </Button>
-                </DropdownMenuTrigger>
-
-                <DropdownMenuContent align="end">
-                  <DropdownMenuItem>Edit</DropdownMenuItem>
-                  <DropdownMenuItem className="text-destructive">Delete</DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-          ))}
+            );
+          })}
         </CardContent>
       </Card>
     </div>
