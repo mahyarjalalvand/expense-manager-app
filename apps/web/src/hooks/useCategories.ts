c84d8f2368e631178e43160b1a0d0290f1f3187c
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-09-21 19:43:14 +0330

    feat(web): add custom hook for create category

--- apps/web/src/hooks/useCategories.ts
+++ apps/web/src/hooks/useCategories.ts
@@ -1,9 +1,21 @@
-import { getCategories } from "@/api/category";
-import { useQuery } from "@tanstack/react-query";
+import { createCategory, getCategories } from "@/api/category";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const useCategories = () => {
   return useQuery({
     queryKey: ["categories"],
     queryFn: getCategories,
+  });
+};
+
+export const useCreateCategory = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: createCategory,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["categories"],
+      });
+    },
   });
 };
